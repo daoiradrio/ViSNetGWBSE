b35daa657891f72fd76acf1d05c269af00ac6beb
Dario Baum <dario-cbr@hotmail.de>
2025-07-04 16:41:46 +0200

    Adding script for different handling of OMol25

--- data/memmap_omol25.py
+++ data/memmap_omol25.py
@@ -23,21 +23,11 @@ class OMol25Dataset(Dataset):
         super().__init__()
         self.split_set = split_set
         self.num_data = int(np.loadtxt(f"OMol25/{self.split_set}/num_data.dat"))
-        N_memmap = np.memmap(f"OMol25/{self.split_set}/N.npy", dtype="int32", mode="r", shape=(self.num_data,))
-        Z_memmap = np.memmap(f"OMol25/{self.split_set}/Z.npy", dtype="int32", mode="r", shape=(self.num_data, N_MAX))
-        R_memmap = np.memmap(f"OMol25/{self.split_set}/R.npy", dtype="float32", mode="r", shape=(self.num_data, N_MAX, 3))
-        M_memmap = np.memmap(f"OMol25/{self.split_set}/M.npy", dtype="bool", mode="r", shape=(self.num_data, N_MAX))
-        E_memmap = np.memmap(f"OMol25/{self.split_set}/HOMO.npy", dtype="float32", mode="r", shape=(self.num_data,))
-        self.N = torch.from_numpy(np.asarray(N_memmap))
-        self.Z = torch.from_numpy(np.asarray(Z_memmap))
-        self.R = torch.from_numpy(np.asarray(R_memmap))
-        self.M = torch.from_numpy(np.asarray(M_memmap))
-        self.E = torch.from_numpy(np.asarray(E_memmap))
-        del N_memmap
-        del Z_memmap
-        del R_memmap
-        del M_memmap
-        del E_memmap
+        self.Z = np.memmap(f"OMol25/{self.split_set}/Z.npy", dtype="int32", mode="r", shape=(self.num_data, N_MAX))
+        self.R = np.memmap(f"OMol25/{self.split_set}/R.npy", dtype="float32", mode="r", shape=(self.num_data, N_MAX, 3))
+        self.M = np.memmap(f"OMol25/{self.split_set}/M.npy", dtype="bool", mode="r", shape=(self.num_data, N_MAX))
+        self.N = np.memmap(f"OMol25/{self.split_set}/N.npy", dtype="int32", mode="r", shape=(self.num_data,))
+        self.E = np.memmap(f"OMol25/{self.split_set}/HOMO.npy", dtype="float32", mode="r", shape=(self.num_data,))
 
 
     def __len__(self):
@@ -45,11 +35,11 @@ class OMol25Dataset(Dataset):
 
 
     def __getitem__(self, i):
-        Z = self.Z[i, :]
-        R = self.R[i, :, :]
-        M = self.M[i, :]
-        N = [self.N[i]]
-        E = [self.E[i]]
+        Z = torch.from_numpy(self.Z[i, :].copy())
+        R = torch.from_numpy(self.R[i, :, :].copy())
+        M = torch.from_numpy(self.M[i, :].copy())
+        N = torch.tensor([self.N[i]])
+        E = torch.tensor([self.E[i]])
 
         Z = Z[M]
         R = R[M, :]
